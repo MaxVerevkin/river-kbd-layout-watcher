5a90b24cad0b2beaf007f695d333eefe867f7350
MaxVerevkin <maxxverrr@gmail.com>
2023-01-01 22:43:12 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# river-kbd-layout-watcher
+
+A simple program that prints current keyboard layout whenever it changes.
+
+Depends on https://github.com/riverwm/river/pull/753
---
+++ src/main.rs
@@ -0,0 +1,62 @@
+mod protocol {
+    use wayrs_client;
+    pub use wayrs_client::protocol::*;
+    wayrs_scanner::generate!("river-status-unstable-v1.xml");
+}
+use protocol::*;
+
+use wayrs_client::connection::Connection;
+use wayrs_client::global::GlobalsExt;
+use wayrs_client::proxy::{Dispatch, Dispatcher};
+use wayrs_client::socket::IoMode;
+
+use std::convert::Infallible;
+
+fn main() {
+    let mut conn = Connection::connect().unwrap();
+    let globals = conn.blocking_collect_initial_globals().unwrap();
+
+    let seat: WlSeat = globals.bind(&mut conn, 1..=1).unwrap();
+    let status_mgr: ZriverStatusManagerV1 = globals.bind(&mut conn, 5..=5).unwrap();
+    let _seat_status = status_mgr.get_river_seat_status(&mut conn, seat);
+    status_mgr.destroy(&mut conn);
+
+    let mut state = State;
+
+    loop {
+        conn.flush(IoMode::Blocking).unwrap();
+        conn.recv_events(IoMode::Blocking).unwrap();
+        conn.dispatch_events(&mut state).unwrap();
+    }
+}
+
+struct State;
+
+impl Dispatcher for State {
+    type Error = Infallible;
+}
+
+impl Dispatch<ZriverSeatStatusV1> for State {
+    fn event(
+        &mut self,
+        _: &mut Connection<Self>,
+        _: ZriverSeatStatusV1,
+        event: zriver_seat_status_v1::Event,
+    ) {
+        use zriver_seat_status_v1::Event;
+        match event {
+            Event::KeyboardLayout(layout) => {
+                println!("{}", String::from_utf8_lossy(layout.as_bytes()));
+            }
+            Event::KeyboardLayoutClear => println!(),
+            _ => (),
+        }
+    }
+}
+
+// Don't care
+impl Dispatch<WlRegistry> for State {}
+impl Dispatch<WlSeat> for State {}
+
+// No events
+impl Dispatch<ZriverStatusManagerV1> for State {}
